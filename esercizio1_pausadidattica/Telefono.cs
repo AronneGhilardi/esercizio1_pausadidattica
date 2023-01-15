234736f3676de4187e9ab9b0f5974a2c728c6e24
AronneGhilardi <aronneghilardi05@gmail.com>
2023-01-15 15:38:06 +0100

    versione aggiornata

--- esercizio1_pausadidattica/Telefono.cs
+++ esercizio1_pausadidattica/Telefono.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace esercizio1_pausadidattica
 {
@@ -13,15 +15,39 @@ namespace esercizio1_pausadidattica
         private string _modello;
         private double _credito;
         private double _creditomin;
+        private string[] _listanera;
 
         public Telefono(string id, string produttore, string modello, double credito, double creditomin)
         {
             Id = id;
             Produttore = produttore;
             Modello = modello;
-            Credito = credito;
             CreditoMin = creditomin;
+            Credito = credito;
+            _listanera = new string[50];
         }
+
+        public Telefono(string id, string modello, double credito, double creditomin) : this(id, "NA", modello, credito, creditomin)
+        {
+
+        }
+
+        public Telefono(string id, double credito, double creditomin) : this(id, "NA", "NA", credito, creditomin)
+        {
+
+        }
+
+        public Telefono(string id) : this(id, "NA", "NA", 0, 0)
+        {
+
+        }
+
+        public Telefono() : this("NA", "NA", "NA", 0, 0)
+        {
+
+        }
+
+
 
         public string Id
         {
@@ -84,7 +110,14 @@ namespace esercizio1_pausadidattica
         {
             set
             {
-                _credito = value;
+                if ( value > CreditoMin)
+                {
+                    _credito = value;
+                }
+                else
+                {
+                    throw new Exception("Credito minimo raggiunto!");
+                }
             }
             get
             {
@@ -96,14 +129,7 @@ namespace esercizio1_pausadidattica
         {
             set
             {
-                if (value >= -10)
-                {
-                    _creditomin = value;
-                }
-                else
-                {
-                    throw new Exception("Inesistente!");
-                }
+                _creditomin = value;
             }
             get
             {
@@ -123,11 +149,24 @@ namespace esercizio1_pausadidattica
             }
         }
 
-        public void Avviare()
+        public void Avviare(string nr)
         {
-            if (Credito >= 0)
+            if (Credito >= CreditoMin)
             {
-                Digitare();
+                Digitare(nr);
+                switch(Comporre(nr))
+                {
+                    case 1:
+                        Credito = Credito -1;
+                        break;
+
+                    case 2:
+                        Credito = Credito - 10;
+                        break;
+
+                    case 3:
+                        throw new Exception("Numero invalido!");
+                }
             }
             else
             {
@@ -135,13 +174,98 @@ namespace esercizio1_pausadidattica
             }
         }
 
-        public void Digitare()
+        public void Digitare(string nr)
         {
+            string pattern = @"[0-9+#*]*";
+            if (!Regex.IsMatch(nr, pattern))
+            {
+                throw new Exception("Errore nella digitazione del numero di telfono!");
+            }
+        }
+
+        public int Comporre(string nr)
+        {
+            string nazionale = @"^[0-9]{3,4}[0-9]{7,9}$";
+            string internazionale = @"^\+[0-9]{2}[0-9]{3,4}[0-9]{7,9}$";
+            if (Regex.IsMatch(nr,nazionale))
+            {
+                return 1; //NAZIONALE
+            }
+            else if (Regex.IsMatch(nr, internazionale))
+            {
+                return 2; //INTERNAZIONALE
+            }
+            else 
+            {
+                return 3; //NON VALIDO
+            }
+        }
+
+        int counter = 0;
+
+        public void AggiungereNero(string nr)
+        {
+            Digitare(nr);
+            _listanera[counter] = nr;
+            counter++;
 
         }
 
+        public void EliminareNero(string  nr)
+        {
+            int pos = -1;
 
+            for (int i = 0; i < 50; i++)
+            {
+                if (_listanera[i] == nr)
+                {
+                    pos = i;
+                    break;
+                }
+            }
+
+            if (pos != -1)
+            {
+                _listanera[pos] = null;
+            } 
+            else
+            {
+                throw new Exception("Non trovato");
+            }   
+        }
+
+        public string[] VisualizzareNero()
+        {
+            return _listanera;
+        }
+
+        public override string ToString()
+        {
+            return this.Id + ";" + this.Produttore + ";" + this.Modello + ";" + this.Credito + ";" + this.CreditoMin;
+        }
+
+        public bool Equals(Telefono x)
+        {
+            if (x == null)
+            {
+                return false;
+            }
+            if (this == x)
+            {
+                return true;
+            }
+
+            return (this.Id == x.Id);
+        }
+
+        public Telefono Clone()
+        {
+            return new Telefono(this);
+        }
+
+        protected Telefono(Telefono other) : this(other.Id, other.Produttore, other.Modello, other.Credito, other.CreditoMin)
+        {
+
+        }
     }
-
-    
 }
